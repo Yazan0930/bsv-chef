1deb00bf0e2a4a7ac3742dd7a3efdebd3d167064
Yazan0930 <109805307+Yazan0930@users.noreply.github.com>
2023-05-22 12:00:26 +0200

    Update test_get_receipe.py

--- backend/test/unit/test_get_receipe.py
+++ backend/test/unit/test_get_receipe.py
@@ -1,6 +1,52 @@
 import pytest
+from src.controllers.receipecontroller import ReceipeController
+from src.static.diets import Diet
+from unittest.mock import patch, MagicMock
 
-# add your test case implementation here
-@pytest.mark.unit
-def test():
-    assert False
+@pytest.fixture
+def receipe_controller():
+    # Set up 
+    Controller = MagicMock()
+    return ReceipeController(Controller)
+
+@patch('src.controllers.receipecontroller.calculate_readiness')
+def test_get_receipe_readiness_complies_with_diet_and_readiness_greater_than_threshold(mock_calculate_readiness, receipe_controller):
+    # Arrange
+    receipe = { "name": "Test Receipe", "diets": ["vegan"], "ingredients": [...] }
+    available_items = { "ingredient1": 2, "ingredient2": 1 }
+    diet = Diet.VEGAN
+    mock_calculate_readiness.return_value = 0.3
+
+    # Act
+    readiness = receipe_controller.get_receipe_readiness(receipe, available_items, diet)
+
+    # Assert
+    assert readiness > 0.1
+
+@patch('src.controllers.receipecontroller.calculate_readiness')
+def test_get_receipe_readiness_complies_with_diet_and_readiness_less_than_threshold(mock_calculate_readiness, receipe_controller):
+    # Arrange
+    receipe = { "name": "Test Receipe", "diets": ["vegan"], "ingredients": [...] }
+    available_items = { "ingredient1": 0, "ingredient2": 1 }
+    diet = Diet.VEGAN
+    mock_calculate_readiness.return_value = 0.05
+
+    # Act
+    readiness = receipe_controller.get_receipe_readiness(receipe, available_items, diet)
+
+    # Assert
+    assert readiness is None
+
+@patch('src.controllers.receipecontroller.calculate_readiness')
+def test_get_receipe_readiness_does_not_comply_with_diet(mock_calculate_readiness, receipe_controller):
+    # Arrange
+    receipe = { "name": "Test Receipe", "diets": ["vegan"], "ingredients": [...] }
+    available_items = { "ingredient1": 2, "ingredient2": 1 }
+    diet = Diet.VEGETARIAN
+    mock_calculate_readiness.return_value = 0.3
+
+    # Act
+    readiness = receipe_controller.get_receipe_readiness(receipe, available_items, diet)
+
+    # Assert
+    assert readiness is None
